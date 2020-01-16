6a765f0b1dfb543a7e1304ba3617ade5ab0bdab7
fersalazarbox <lsalazaro1400@alumno.ipn.mx>
2020-01-16 17:32:32 -0600

    Avance a la programación de eventos para Erlang

--- cells.py
+++ cells.py
@@ -45,17 +45,21 @@ class Usuario(object):
         return random.expovariate(1 / Mu) # Distribución exponencial
     #ListausuariosMoviles=[Id, posicion, BS, M]
     ListaUsuariosMoviles=[]
+    capacidadRecurso = 10
 
 class Simulacion(object):
 
     """Esta clase representa a una Simulación de eventos discretos"""
     contadorLlegadas = 0
+    contadorLlegadasC1=0
     contadorSalidas = 0
+    contadorBloqueoXRecurso = 0
     umbralArribos = 0 # Número de arribos a simular (CONDICIÓN DE PARO)
     # Lista de eventos de las llegadas de usuarios
     Llegadas= []
     # Lista de eventos de las salidas de usuarios
     Salidas= []
+
 
 def simulacionEventosDiscretos(entorno, usuario, simulacion, estacionesbase, terminarSimulacion):
 
@@ -86,10 +90,9 @@ def simulacionEventosDiscretos(entorno, usuario, simulacion, estacionesbase, ter
         bs_position.append([(mth.sqrt(3 * cluster_size) * r_cell * np.cos(theta[i] + theta_N[ind])),
                             (mth.sqrt(3 * cluster_size) * r_cell * np.sin(theta[i] + theta_N[ind]))])
 
-    for i in range (0, num_celdas+1):
+    estacionesbase.ListaEstacionesBase.append([0, [0, 0], 0, [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]])
+    for i in range (0, num_celdas):
         estacionesbase.ListaEstacionesBase.append([i, bs_position[i], 0, [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]])
-
-
 
     # Creación de figura a plotear
     fig, ax = plt.subplots(1)
@@ -120,7 +123,7 @@ def simulacionEventosDiscretos(entorno, usuario, simulacion, estacionesbase, ter
         #ax.scatter(bs_position[j][0] / 2, bs_position[j][1] / 2, c='k', alpha=0.5)
 
     # Ploteo de figura
-    plt.show()
+    #plt.show()
 
     while True:
         # Se calendariza la llegada de un usuario, que es equivalente a su petición de servicio
@@ -153,6 +156,7 @@ def simulacionEventosDiscretos(entorno, usuario, simulacion, estacionesbase, ter
                     celda_a_posicionar = random.randint(0, num_celdas)
 
                     if celda_a_posicionar == 0:
+                        simulacion.contadorLlegadasC1=simulacion.contadorLlegadasC1+1
                         # Llegará a la celda central
                         # y se establecen los moviles dentro del sector seleccionado
                         des_user_beta = np.random.uniform(0, 1) * phi_BW[sec - 1] + phi_center[sec - 1][sector - 1]
@@ -162,8 +166,24 @@ def simulacionEventosDiscretos(entorno, usuario, simulacion, estacionesbase, ter
                         # Ubicacion [X,Y] del móvil en la celda central
                         des_user_position = [np.cos(des_user_beta) * des_user_r, np.sin(des_user_beta) * des_user_r]
                         ax.scatter(des_user_position[0], des_user_position[1], c='b', alpha=0.3)
-                        usuario.ListaUsuariosMoviles.append([i, 0, des_user_position, False])
-                        estacionesbase.ListaEstacionesBase[0][3] = [i, des_user_r]
+
+                        usuario.ListaUsuariosMoviles.append([simulacion.Llegadas[i].value, 0, des_user_position, False])
+                        # Verificar si hay disponibilidad
+                        if estacionesbase.ListaEstacionesBase[celda_a_posicionar][2] < usuario.capacidadRecurso:
+                            print("SI hay recursos, se asignará recurso a ", simulacion.Llegadas[i].value)
+                            for j in range(0, usuario.capacidadRecurso):
+                                # estacion base 0 / lista recursos 3 / recurso i
+                                if estacionesbase.ListaEstacionesBase[0][3][j] == [0, 0]:
+                                    #asignar conexion
+                                    estacionesbase.ListaEstacionesBase[0][3][j] = [simulacion.Llegadas[i].value, des_user_r]
+                                    #aumentar contador
+                                    estacionesbase.ListaEstacionesBase[0][2] = estacionesbase.ListaEstacionesBase[0][2]+1
+                                    break
+
+                        elif estacionesbase.ListaEstacionesBase[celda_a_posicionar][2] == usuario.capacidadRecurso:
+                            print("No hay suficientes recursos")
+                            simulacion.contadorBloqueoXRecurso = simulacion.contadorBloqueoXRecurso + 1
+
 
                     else:
                         # Llegará a cualquiera de las 6 celdas co canal interferentes
@@ -174,13 +194,28 @@ def simulacionEventosDiscretos(entorno, usuario, simulacion, estacionesbase, ter
                         # Ubicacion [X,Y] de los móviles en las celdas co canal
                         co_ch_user_position = [co_ch_user_r * np.cos(co_ch_user_beta) + bs_position[celda_a_posicionar - 1][0], co_ch_user_r * np.sin(co_ch_user_beta) + bs_position[celda_a_posicionar - 1][1]]
                         ax.scatter(co_ch_user_position[0], co_ch_user_position[1], c='r', alpha=0.3)
-                        usuario.ListaUsuariosMoviles.append([simulacion.contadorLlegadas, celda_a_posicionar, co_ch_user_position, False])
+                        usuario.ListaUsuariosMoviles.append([simulacion.Llegadas[i].value, celda_a_posicionar, co_ch_user_position, False])
+                        # Verificar si hay disponibilidad
+                        if estacionesbase.ListaEstacionesBase[celda_a_posicionar][2] < usuario.capacidadRecurso:
+                            print("SI hay recursos, se asignará recurso a ", simulacion.Llegadas[i].value)
+                            for j in range(0, usuario.capacidadRecurso):
+                                # estacion base 0 / lista recursos 3 / recurso i
+                                if estacionesbase.ListaEstacionesBase[celda_a_posicionar][3][j] == [0, 0]:
+                                    # asignar conexion
+                                    estacionesbase.ListaEstacionesBase[celda_a_posicionar][3][j] = [simulacion.Llegadas[i].value, co_ch_user_r]
+                                    # aumentar contador
+                                    estacionesbase.ListaEstacionesBase[celda_a_posicionar][2] = estacionesbase.ListaEstacionesBase[celda_a_posicionar][2] + 1
+                                    break
+                        #elif estacionesbase.ListaEstacionesBase[celda_a_posicionar][2] == usuario.capacidadRecurso:
+                        #    print("No hay suficientes recursos")
+                        #    simulacion.contadorBloqueoXRecurso = simulacion.contadorBloqueoXRecurso + 1
 
 
                     del simulacion.Llegadas[i]
                     break
 
         entorno.process(calendarizarSalida(entorno, usuario, simulacion, terminarSimulacion))
+
 
 
 def calendarizarSalida(entorno, usuario, simulacion, terminarSimulacion):
@@ -194,6 +229,18 @@ def calendarizarSalida(entorno, usuario, simulacion, terminarSimulacion):
             if simulacion.Salidas[i].processed:
                 print(entorno.now, " Salida de usuario", simulacion.Salidas[i].value)
                 # Quitar usuario del plano
+                # Identificar a usuario como muerto
+                usuario.ListaUsuariosMoviles[simulacion.Salidas[i].value][3] = True
+                # Liberar recursos
+                for j in range (0, usuario.capacidadRecurso):
+                    # buscar conexion usuario
+                    if estacionesbase.ListaEstacionesBase[usuario.ListaUsuariosMoviles[simulacion.Salidas[i].value][1]][3][j][0] == simulacion.Salidas[i].value:
+                        # reiniciarla con 0
+                        estacionesbase.ListaEstacionesBase[usuario.ListaUsuariosMoviles[simulacion.Salidas[i].value][1]][3][j] = [0, 0]
+                        # bajar contador
+                        estacionesbase.ListaEstacionesBase[usuario.ListaUsuariosMoviles[simulacion.Salidas[i].value][1]][2] = estacionesbase.ListaEstacionesBase[usuario.ListaUsuariosMoviles[simulacion.Salidas[i].value][1]][2] - 1
+                        break
+
                 del simulacion.Salidas[i]
                 break
 
@@ -205,7 +252,7 @@ def condiciondeParo(terminarSimulacion, simulacion):
 
 # Inicialización de la simulación
 entorno = simpy.Environment()
-Lambda = 2
+Lambda = 200
 Mu = 1
 # Creacion de objeto clase Usuario
 usuario = Usuario(entorno, Lambda, Mu)
@@ -214,7 +261,7 @@ estacionesbase = EstacionBase(entorno, 70)
 
 # Creacion de objeto clase Simulación
 simulacion = Simulacion()
-simulacion.umbralArribos = 200
+simulacion.umbralArribos = 1000
 
 terminarSimulacion = simpy.events.Event(entorno)
 
