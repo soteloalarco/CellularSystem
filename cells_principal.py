c43fa1c350016941424492f6b4fcfcf572003c44
fersalazarbox <lsalazaro1400@alumno.ipn.mx>
2020-01-21 15:08:20 -0600

    Implementaciòn de multiprocesamiento usando multiprocessing y midiendo tiempos con un temporizador

--- cells_principal.py
+++ cells_principal.py
@@ -7,28 +7,22 @@ import matplotlib.pyplot as plt
 from scipy.special import factorial
 import subprocess
 import sys
+import multiprocessing as mp
 
-
-class Simulacion():
-    """Esta clase representa una simulación"""
-    a = []
-    umbralTopeArribos = 0
-    y = []
-    numceldas = 7
 
 
 # Simulacion para un conjunto de valores de a
-def simulacion_a(simulacion):
-    output = []
-    for a1 in simulacion.a:
-        output.append(subprocess.check_output('cells.py '+str(a1)+' '+str(simulacion.umbralTopeArribos), shell=True))
-        # os.system(".py "+str(p))
+class Simulacion():
+    """Esta clase representa una simulación"""
+    a = []
+    y = []
+    numceldas = 0
 
-    # Conversion del formato de salida
-    salida = []
-    for i in range(len(simulacion.a)):
-        salida.append(output[i].decode(sys.stdout.encoding))
-        simulacion.y.append(float(salida[i].rstrip()))
+# Simulacion para un conjunto de valores de a
+def simulacion_a(a1, umbralTopeArribos=15000):
+    output = subprocess.check_output('cells.py '+str(a1)+' '+str(umbralTopeArribos), shell=True)
+    output = output.decode(sys.stdout.encoding)
+    return output.rstrip()
 
 
 
@@ -55,21 +49,40 @@ def graficasProbBloq(simulacion):
     plt.show()
 
 
-#Definicion de Parámetros de la simulación
 
-simulacion= Simulacion() #Creación de objeto de clase Simulación
-a1 = input('Ingresa el valor inicial de a (a>0):        ')
-a2 = input('Ingresa el valor final de a:                ')
-a3 = input('Ingresa el número de muestras a generar:    ')
-simulacion.a = np.linspace(float(a1), float(a2), int(a3))#(0.01, 22, 100)
-simulacion.umbralTopeArribos = int(input('Ingresa el umbral tope de arribos a generar: '))#10000
-#os.system('cls')
-print("  Simulando  ...")
-inicio = timer()
-#Ejecución de funciones
-simulacion_a(simulacion)
-graficasProbBloq(simulacion)
+if __name__ == '__main__':
+    # Definicion de Parámetros de la simulación
+    capacidad = 0
+    umbralTopeArribos = 0
+    output = []
 
-fin = timer()
-tiempo_TotalSimulacion = fin - inicio
-print('Tiempo total de simulacion en segundos: ', tiempo_TotalSimulacion, '  en minutos: ', tiempo_TotalSimulacion/60, '  y en horas: ', tiempo_TotalSimulacion/60/60)
+    simulacion = Simulacion()  # Creación de objeto de clase Simulación
+    simulacion.numceldas=7
+    a1 = input('Ingresa el valor inicial de a (a>0):        ')
+    a2 = input('Ingresa el valor final de a:                ')
+    a3 = input('Ingresa el número de muestras a generar:    ')
+    simulacion.a = np.linspace(float(a1), float(a2), int(a3))  # (0.01, 22, 100)
+
+
+    inicio = timer()
+    print("  Simulando  ...")
+    # Multiprocesamiento
+
+    pool = mp.Pool(mp.cpu_count())
+
+    results = pool.map(simulacion_a, [a1 for a1 in simulacion.a])
+
+    pool.close()
+
+    #Conversion del formato de salida
+
+    for i in range(len(results)):
+       simulacion.y.append(float(results[i]))
+
+    graficasProbBloq(simulacion)
+
+
+    fin = timer()
+    tiempo_TotalSimulacion = fin - inicio
+    print('Tiempo total de simulacion en segundos: ', tiempo_TotalSimulacion, '  en minutos: ', tiempo_TotalSimulacion/60, '  y en horas: ', tiempo_TotalSimulacion/60/60)
+
